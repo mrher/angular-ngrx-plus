a99fe659a9d4107e11c8bb3858dd0cc995c55ad6
Serge <mrhersergik@gmail.com>
2023-11-29 13:25:43 +0100

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-plus-ngrx';
+  increase() {
+
+  }
+
+  decrease() {
+
+  }
+
+  clear() {
+
+  }
 }
